1b3c0efd8c19c4587ff0902c6730a771e2729226
AyeshaDevop <ashiiayesha123@gmail.com>
2024-09-29 08:05:36 -0700

    print Hello word

---
+++ main.py
@@ -0,0 +1,43 @@
+           #string
+
+print("hello world")
+
+# sring : anything in inverted comes is called string ( ' ' , " " , '''  ''')
+
+name2 = str ("Ayesha")
+
+
+
+
+
+#for example
+# string = str
+# #Type of string
+# 3 type of string 
+# 1.  Single = 'Ayesha'
+# 2.  double = "Ayesha"
+# 3.  Triple = '''Ayesha''' # multi-lines
+
+
+
+# string ('Ayesha')
+
+
+
+# string ("Ayesha")
+# string 
+
+#string Operations
+1. #concatenation
+2. #Repetition 
+3. #Indexing
+4. #Slicing 
+5. #Splitting
+6. #Joining
+
+    
+#concatenation 
+#concatenation : + ( "hello" , "world")
+#for Example
+
+name1 = "ayesha"
