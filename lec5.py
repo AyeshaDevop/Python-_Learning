2ecdf5a4267b0b771d56c2ae058e2320b4624bed
AyeshaDevop <ashiiayesha123@gmail.com>
2024-10-08 15:15:51 +0500

    Loops

---
+++ lec5.py
@@ -0,0 +1,22 @@
+#Loops in python
+#(for loop and while loop)
+#if loop
+# for a  in range(1, 6):
+#     print(a)
+#while loop
+# a = 1
+# while a <= 5:
+#     print(a)
+#     a += 1
+    
+#infinite loop
+# while True:
+#     print("This will print forever!")
+
+#Breaking  out loop
+# for a  in range(1, 10):
+#     if a  == 5:
+#         break
+#     print(a)
+
+
