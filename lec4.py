5306550ae7cfac0075ab94f64aa6bca8b157b74f
AyeshaDevop <ashiiayesha123@gmail.com>
2024-10-08 14:56:42 +0500

    Dic_set

--- lec4.py
+++ lec4.py
@@ -32,28 +32,28 @@
 
 # print(type(set1))
 
-# Creating sets
-set_a = {1, 2, 3, 4, 5}
-set_b = {4, 5, 6, 7, 8}
+# #  sets
+# set_1 = {1, 2, 3, 4, 5}
+# set_2 = {3, 4, 5, 6, 7}
 
-# Add an element
-set_a.add(6)
-print("After adding 6 to set_a:", set_a)
+# # Add an element
+# set_1.add(6)
+# print("After adding 6 to set_1:", set_1)
 
-# Remove an element
-set_a.remove(1)
-print("After removing 1 from set_a:", set_a)
+# # Remove an element
+# set_1.remove(1)
+# print("After removing 1 from set_1:", set_1)
 
-# Union of sets (all unique elements from both sets)
-union_set = set_a.union(set_b)
-print("Union of set_a and set_b:", union_set)
+# # Union of sets (all unique elements from both sets)
+# union_set = set_1.union(set_2)
+# print("Union of set_1 and set_2:", union_set)
 
-# Intersection of sets (common elements)
-intersection_set = set_a.intersection(set_b)
-print("Intersection of set_a and set_b:", intersection_set)
+# # Intersection of sets (common elements)
+# intersection_set = set_1.intersection(set_2)
+# print("Intersection of set_1 and set_2:", intersection_set)
 
-# Difference of sets (elements in set_a but not in set_b)
-difference_set = set_a.difference(set_b)
-print("Difference of set_a and set_b:", difference_set)
+# # Difference of sets (elements in set_a but not in set_b)
+# difference_set = set_1.difference(set_2)
+# print("Difference of set_1 and set_2:", difference_set)
 
 
