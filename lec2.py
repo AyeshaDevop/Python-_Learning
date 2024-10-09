8ef4f4e39aad627e19de27665b7dbf15fed28817
AyeshaDevop <ashiiayesha123@gmail.com>
2024-10-10 02:44:23 +0500

    ok

--- lec2.py
+++ lec2.py
@@ -1,32 +1,31 @@
- string
+ #string
 
-str = " ayesha "
-Basic operations
-( /n = for next line ) # ( /t = for space  ) 
-for Example
+str1="ayesha"
+# Basic operations
+# ( /n = for next line ) # ( /t = for space  ) 
+#for Example
 
-String examples
+#String examples
 name1 = "ayesha"
 name2 = "asad"
 
-Basic operations
-\n for new line
-\t for tab/space
+#Basic operations
+# \n for new line
+# \t for tab/space
 
-Concatenation of two strings
+#Concatenation of two strings
 concatenated_string = name1 + " " + name2  # Adding a space between the names
 #"hello" , "world" # = "helloworld"
-
 
 # Printing results
 print("Concatenated String:\n", concatenated_string)  # New line before the string
 print("Names with tab:\n\t", name1, "\n\t", name2)    # Printing with a tab space=("ayesha","asad")
-Concatenation
+#Concatenation
 Books = ("eng","urdu")
 print(Books)
-Length of string
+#Length of string
 
-str = "Ayesha"
+str2 = "Ayesha"
 print(len)
-print(len(str))
+print(len(str2))
 
