e005698ca1507feb6b9334760cd43523ecf70718
AyeshaDevop <ashiiayesha123@gmail.com>
2024-10-11 00:18:25 +0500

    ok

--- lec3.py
+++ lec3.py
@@ -10,29 +10,28 @@ marks=[12 , 56.5 , 46.9 , 43]
 print(marks)
 
 # 1.Empty list :
-# list = []
+list1 = []
 
 # #2. Integer list
-# list = [1, 2, 3, 4, 5]
+list2 = [1, 2, 3, 4, 5]
 
 # #3. String list:
-# list = ['apple', 'banana', 'mango']
+list3 = ['apple', 'banana', 'mango']
 
 # #4. Mixed-type list:
-# list = [1, "ali", "ahmad", 3.4, True]
+list4 = [1, "ali", "ahmad", 3.4, True]
 
 
 #Indexing and Slicing in lists
 
 #1. Accessing elements:
-# list[0]    # returns the first element
+list5=[0]    # returns the first element
 # #2. Slicing: my_list[1:3] returns a subset of elements
 # #3. Negative indexing:
-# list[-1]         #returns the last element
+list6=[-1]         #returns the last element
 
 
 #List Methods
-
 
 # 1. Append: 
 # list.append('new_element')
@@ -47,17 +46,14 @@ print(marks)
 # 6. Reverse:
 #  my_list.reverse()
 
-
-
-
-
 # Real-World Examples
 
-
-# 1. Student marks: marks = [90, 85, 95, 88]
-# 2. Shopping fruit: fruit  = ['apple', 'banana', 'orange']
-# 3. 
-# students records: records = [{'name': 'ali', 'age': 16}, {'name': 'Ahmad', 'age': 17}]
+Student1 marks5 :
+marks 5= [90, 85, 95, 88] # type: ignore
+Shopping2 fruit:
+fruit  = ['apple', 'banana', 'orange']
+students3 records:
+records = [{'name': 'ali', 'age': 16}, {'name': 'Ahmad', 'age': 17}]
 
 
 
