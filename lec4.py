c11ef4370f97a6945f81b05a1918878d592d095b
AyeshaDevop <ashiiayesha123@gmail.com>
2024-10-11 00:24:14 +0500

    ok

--- lec4.py
+++ lec4.py
@@ -1,59 +1,53 @@
 #Dictionary
 # Create a dictionary
-# student = {
-#     "name": "Ayesha",
-#     "age": 19,
-#     "subjects": ["che","phy","eng"]}
+student = {
+"name": "Ayesha",
+"age": 19,
+"subjects": ["che","phy","eng"]}
 
-# # Access a value by its key
-# print(student["name"])  # Output: Ayesha
+print(student["name"])  # Output: Ayesha
 
 
 # # Add a new key-value pair
-# student["school"] = "Peak school"
+student["school"] = "Peak school"
 
 # # Update a value
-# student["grade"] = "13"
+student["grade"] = "13"
 
 # # Delete a key-value pair
-# del student["age"]
+del student["age"]
 
 # # Print the updated dictionary
-# print(student)
+print(student)
  
  #Set in python
 # Using curly braces
-# set1 = {1, 2, 3, 4, 5}
-# print(set1)
+set1 = {1, 2, 3, 4, 5}
+print(set1)
 
 # Using the set() function
-# set2 = set([2, 4, 6, 8])
-# print(set2)
+set2 = set([2, 4, 6, 8])
+print(set2)
 
 # print(type(set1))
 
 # #  sets
-# set_1 = {1, 2, 3, 4, 5}
-# set_2 = {3, 4, 5, 6, 7}
+set_1 = {1, 2, 3, 4, 5}
+set_2 = {3, 4, 5, 6, 7}
 
-# # Add an element
-# set_1.add(6)
-# print("After adding 6 to set_1:", set_1)
+set_1.add(6)
+print("After adding 6 to set_1:", set_1)
 
-# # Remove an element
-# set_1.remove(1)
-# print("After removing 1 from set_1:", set_1)
+set_1.remove(1)
+print("After removing 1 from set_1:", set_1)
 
-# # Union of sets (all unique elements from both sets)
-# union_set = set_1.union(set_2)
-# print("Union of set_1 and set_2:", union_set)
+union_set = set_1.union(set_2)
+print("Union of set_1 and set_2:", union_set)
 
-# # Intersection of sets (common elements)
-# intersection_set = set_1.intersection(set_2)
-# print("Intersection of set_1 and set_2:", intersection_set)
+intersection_set = set_1.intersection(set_2)
+print("Intersection of set_1 and set_2:", intersection_set)
 
-# # Difference of sets (elements in set_a but not in set_b)
-# difference_set = set_1.difference(set_2)
-# print("Difference of set_1 and set_2:", difference_set)
+difference_set = set_1.difference(set_2)
+print("Difference of set_1 and set_2:", difference_set)
 
 
