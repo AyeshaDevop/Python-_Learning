bf224d703e6c1a31e944a6498fd23b458d540336
AyeshaDevop <ashiiayesha123@gmail.com>
2024-10-11 00:27:44 +0500

    ok

--- lec5.py
+++ lec5.py
@@ -4,19 +4,19 @@
 # for a  in range(1, 6):
 #     print(a)
 #while loop
-# a = 1
-# while a <= 5:
-#     print(a)
-#     a += 1
+a = 1
+while a <= 5:
+    print(a)
+    a += 1
     
 #infinite loop
-# while True:
-#     print("This will print forever!")
+while True:
+    print("Learning Python!")
 
 #Breaking  out loop
-# for a  in range(1, 10):
-#     if a  == 5:
-#         break
-#     print(a)
+for a  in range(1, 10):
+    if a  == 5:
+        break
+    print(a)
 
 
